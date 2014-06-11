6236d276bda030c2d722cfbb25b9a23de9bb79c1
Richard Buckle <richardb@sailmaker.co.uk>
2014-06-11 03:03:56 +0100

    Debugging changes
    Make AppDelegate retain storeCoordinator, backgroundContext, and store.
    
    This may or may not be necessary depending on outcome of further tests.

--- CoreDataDebugging/AppDelegate.swift
+++ CoreDataDebugging/AppDelegate.swift
@@ -13,6 +13,9 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
     var window: UIWindow?
+    var storeCoordinator: NSPersistentStoreCoordinator?
+    var backgroundContext: NSManagedObjectContext?
+    var store: NSPersistentStore?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool
     {
@@ -23,53 +26,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate
         self.window!.rootViewController = rootViewController
         self.window!.makeKeyAndVisible()
         
-        setupCoreDataStackAndViolateThreadingContract()
+        setupCoreDataStack()
         
         return true
     }
     
-    func setupCoreDataStackAndViolateThreadingContract()
+    func setupCoreDataStack()
     {
         let objectModelURL = NSBundle.mainBundle().URLForResource("CoreDataDebugging", withExtension: "momd")
         let objectModel: NSManagedObjectModel? = NSManagedObjectModel(contentsOfURL: objectModelURL)
         assert(objectModel)
         
-        let storeCoordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: objectModel)
+        storeCoordinator = NSPersistentStoreCoordinator(managedObjectModel: objectModel)
         assert(storeCoordinator)
         
-        let store: NSPersistentStore? = storeCoordinator!.addPersistentStoreWithType(NSInMemoryStoreType, configuration: nil, URL: nil, options: nil, error: nil)
+        store = storeCoordinator!.addPersistentStoreWithType(NSInMemoryStoreType, configuration: nil, URL: nil, options: nil, error: nil)
         assert(store)
 
         // Set up a managed object context with private queue concurrency
-        let backgroundContext: NSManagedObjectContext? = NSManagedObjectContext(concurrencyType: .PrivateQueueConcurrencyType)
+        backgroundContext = NSManagedObjectContext(concurrencyType: .PrivateQueueConcurrencyType)
         assert(backgroundContext)
-        backgroundContext!.persistentStoreCoordinator = storeCoordinator!
+        backgroundContext!.name = "Background context"
+        backgroundContext!.persistentStoreCoordinator = storeCoordinator
         
-        // Work on the background context without using performBlock:
-        // This should fail because we are violating Core Data's concurrency contract.
-//        let person = NSEntityDescription.insertNewObjectForEntityForName("Person", inManagedObjectContext: backgroundContext!) as NSManagedObject
-//        person.setValue("John Appleseed", forKey: "name")
-//        var potentialSaveError: NSError?
-//        let didSave = backgroundContext!.save(&potentialSaveError)
-//        if (didSave) {
-//            println("Saving successful")
-//        } else {
-//            let saveError = potentialSaveError!
-//            println("Saving failed with error: \(saveError)")
-//        }
-
-        // Work on the background context with using performBlock:
-        // This should work but throws a multithreading violation exception on backgroundContext!.save(&potentialSaveError)
-        backgroundContext!.performBlock {
-            let person = NSEntityDescription.insertNewObjectForEntityForName("Person", inManagedObjectContext: backgroundContext!) as NSManagedObject
+        dispatch_async(dispatch_get_main_queue()) {
+            self.completedCoreDataSetup(self.backgroundContext!)
+        }
+    }
+    
+    func completedCoreDataSetup(backgroundContext: NSManagedObjectContext!) {
+        backgroundContext.performBlock {
+            let person = NSEntityDescription.insertNewObjectForEntityForName("Person", inManagedObjectContext: backgroundContext) as NSManagedObject
             person.setValue("John Appleseed", forKey: "name")
-            var potentialSaveError: NSError?
-            let didSave = backgroundContext!.save(&potentialSaveError)
-            if (didSave) {
-                println("Saving successful")
-            } else {
-                let saveError = potentialSaveError!
-                println("Saving failed with error: \(saveError)")
+            
+            if true {
+                var potentialSaveError: NSError?
+                let didSave = backgroundContext.save(&potentialSaveError)
+                if (didSave) {
+                    println("Saving successful")
+                } else {
+                    let saveError = potentialSaveError!
+                    println("Saving failed with error: \(saveError)")
+                }
             }
         }
     }
